7fe9190d6ec26604a89c0dbc3e0d63c1e075d2a1
Erwin Feng <efeng732@gmail.com>
2020-10-25 00:32:10 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,30 +1,30 @@
 def addition(num1, num2)
   sum = num1 + num2
-  puts sum
+  
   sum
 end
 
 def subtraction(num1, num2)
   diff = num1-num2
-  puts diff
+  
   diff
 end
 
 def division(num1, num2)
   div = num1/num2
-  puts div
+  
   div
 end
 
 def multiplication(num1, num2)
   product = num1 * num2
-  puts product
+  
   product
 end
 
 def modulo(num1, num2)
   remainder = num1 % num2
-  puts remainder
+  
   remainder
 end
 
